ce4d060690dbf87f392488d5ec2cc2909e7bf79a
Gabriel_devs <dcbiel846@gmail.com>
2025-02-08 15:37:54 -0300

    feat: add to access by URL parameter

--- api_rest/views.py
+++ api_rest/views.py
@@ -28,3 +28,23 @@ def get_by_nick(request, nick):
         serializer = UserSerializer(user)
         
         return Response(serializer.data)
+    
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+    if request.method == 'GET':
+        try:
+            if request.GET['user']:
+                user_nickname = request.GET['user']
+                
+                try:
+                    user = user.objects.get(pk=user_nickname)
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
