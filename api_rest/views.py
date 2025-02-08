887b024c85bf035d30e02a0c2d4564737b0e625b
Gabriel_devs <dcbiel846@gmail.com>
2025-02-08 16:02:12 -0300

    feat : adding the last DELETE method to the crud

--- api_rest/views.py
+++ api_rest/views.py
@@ -84,3 +84,12 @@ def user_manager(request):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    # Method Delete
+    if request.method == 'DELETE':
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_nickname'])
+            user_to_delete.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
